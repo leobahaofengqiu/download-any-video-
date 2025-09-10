9ff9f11590c937a6f756255ecd3f6c56ea3ab497
leobahaofengqiu <1421969543qq@gmail.com>
2025-09-10 22:39:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-import os
-import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-def home():
-    return {"msg": "Hello from Railway!"}
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # <- Yahan se Railway ka port pick hoga
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+def root():
+    return {"message": "🚀 FastAPI on Railway is working fine!"}
